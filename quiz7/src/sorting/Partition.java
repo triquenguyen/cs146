581f7a8e5f9696d17ce7df613c9c181c1bf39230
Trique Nguyen <ngngoctridung123@gmail.com>
2023-03-16 14:47:23 -0700

    finished parition

--- quiz7/src/sorting/Partition.java
+++ quiz7/src/sorting/Partition.java
@@ -5,9 +5,17 @@ import java.util.Arrays;
 public class Partition {
 	
 	public static int partitionLomuto(int[] arr,int low, int high) {
-		return 0;
-				   
+		int x = arr[high];
+		int i = low - 1;
 
+		for (int j = low; j <= high - 1; j++) {
+			if (arr[j] <= x) {
+				i++;
+				swap(arr[i], arr[j]);
+			}	
+		}
+		swap(arr[i+1], arr[high]);
+		return i+1;
 	}
 	
 	/***********ANSWER QUESTION HERE*****************/
@@ -17,10 +25,30 @@ public class Partition {
      */
 	
 	public static int partitionHoare(int[] arr,int low, int high) {
-		return 0;
-		
+		int pivot = arr[low];
+		int i = low - 1;
+		int j = high + 1;
+		while (true){
+			if (arr[j] <= pivot) {
+				j--;
+			}
+
+			if (arr[i] >= pivot) {
+				i++;
+			}
+
+			if (i<j) {
+				swap(arr[i], arr[j]);
+			} else {
+				return j;
+			}
+		}
 	}
 	
-
+	public static void swap(int a, int b) {
+		int temp = a;
+		a = b;
+		b = temp;
+	}
 
 }
