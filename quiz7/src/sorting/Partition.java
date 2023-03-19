a7a17c4cb41f89b15ec5fcd84b544fece38e66a5
Trique Nguyen <ngngoctridung123@gmail.com>
2023-03-18 23:14:02 -0700

    added comments

--- quiz7/src/sorting/Partition.java
+++ quiz7/src/sorting/Partition.java
@@ -21,9 +21,8 @@ public class Partition {
 	/*********** ANSWER QUESTION HERE *****************/
 	/*
 	 * What sort of input arrays will enable Hoare’s algorithm to still create
-	 * relatively
-	 * equal size partitions whereas Lomuto’s algorithm will create unequal
-	 * partitions?
+	 * relatively equal size partitions whereas Lomuto’s algorithm will create
+	 * unequal partitions?
 	 */
 
 	public static int partitionHoare(int[] arr, int low, int high) {
