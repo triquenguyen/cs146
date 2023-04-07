a79f0510f13d97641487f80ae955f633d2b81ab4
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-07 00:30:53 -0700

    init quiz9

---
+++ quiz9/src/dynamic/Fibonacci.java
@@ -0,0 +1,14 @@
+package dynamic;
+
+public class Fibonacci {
+	
+	public static long fibMemo(int n) {
+		return 0;
+	}
+    
+	public static long fibBottomUp(int n) {
+		return 0;
+		
+	}
+		
+}
---
+++ quiz9/src/dynamic/MinSumPath.java
@@ -0,0 +1,13 @@
+package dynamic;
+
+public class MinSumPath {
+	
+	public static int minSumPathMemo(int triangle[][]) {
+		return 0;
+	}
+	
+	public static int minSumPathBottomUp(int triangle[][]) {
+		return 0;
+	}
+
+}
