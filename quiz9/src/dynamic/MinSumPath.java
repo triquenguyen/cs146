89fab16d37e569c48a5dfcbe17e61828da96b62b
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-08 00:36:19 -0700

    finished minSum bottom up

--- quiz9/src/dynamic/MinSumPath.java
+++ quiz9/src/dynamic/MinSumPath.java
@@ -1,13 +1,25 @@
 package dynamic;
 
 public class MinSumPath {
-	
+
 	public static int minSumPathMemo(int triangle[][]) {
 		return 0;
 	}
-	
+
 	public static int minSumPathBottomUp(int triangle[][]) {
-		return 0;
+		int[] minSumArr = new int[triangle.length + 1];
+
+		for (int i = triangle.length - 1; i >= 0; i--) {
+			for (int j = 0; j < triangle[i].length; j++) {
+				minSumArr[j] = triangle[i][j] + Math.min(minSumArr[j], minSumArr[j + 1]);
+			}
+		}
+
+		return minSumArr[0];
 	}
 
+	public static void main(String[] args) {
+		int[][] triangle = new int[][] { { 2 }, { 3, 4 }, { 6, 5, 7 }, { 4, 1, 8, 3 } };
+		System.out.println(minSumPathBottomUp(triangle));
+	}
 }
