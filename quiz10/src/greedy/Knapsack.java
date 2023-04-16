2b9611cf7b47d9e4220fef61c740dc9d780e68ae
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-16 15:58:16 -0700

    finished quiz10

--- quiz10/src/greedy/Knapsack.java
+++ quiz10/src/greedy/Knapsack.java
@@ -27,20 +27,16 @@ public class Knapsack {
     }
 
     static ArrayList<Double> fractionalKnapsack(ArrayList<Item> items, double maxWeight) {
-        Collections.sort(items, (item, nextItem) -> Double.compare(item.costPerPound, nextItem.costPerPound));
         ArrayList<Double> itemList = new ArrayList<Double>();
-        double profit = 0;
         double weightLeft = maxWeight;
 
         for (Item item : items) {
             if (weightLeft >= item.weight) {
                 itemList.add(1.0);
-                profit += item.value;
                 weightLeft -= item.weight;
             } else {
                 double itemFraction = weightLeft / item.weight;
                 itemList.add(itemFraction);
-                profit += itemFraction * item.value;
                 weightLeft = 0;
             }
 
@@ -49,27 +45,30 @@ public class Knapsack {
             }
         }
 
-        itemList.add(profit);
+        while (itemList.size() < items.size()) {
+            itemList.add(0.0);
+        }
+
         return itemList;
     }
 
     public static void main(String[] args) {
-        Item item1 = new Item("Trique", 10000, 150);
-        Item item2 = new Item("Phone", 200, 1);
-        Item item3 = new Item("Laptop", 2000, 25);
-        Item item4 = new Item("Monitor", 200, 47);
-        Item item5 = new Item("Kindle", 400, 10);
-        Item item6 = new Item("Headphone", 100, 1);
-        Item item7 = new Item("Cup", 120, 2);
+        Item item1 = new Item("Trique", 10000, 150); // 66.66
+        Item item2 = new Item("Phone", 200, 1); // 200
+        Item item3 = new Item("Laptop", 2000, 25); // 80
+        Item item4 = new Item("Monitor", 200, 47); // 4.25
+        Item item5 = new Item("Kindle", 400, 10); // 40
+        Item item6 = new Item("Headphone", 100, 1); // 100
+        Item item7 = new Item("Cup", 120, 2); // 60
 
         ArrayList<Item> items = new ArrayList<>();
-        items.add(item1);
         items.add(item2);
-        items.add(item3);
-        items.add(item4);
-        items.add(item5);
         items.add(item6);
+        items.add(item3);
+        items.add(item1);
         items.add(item7);
+        items.add(item5);
+        items.add(item4);
 
         ArrayList<Double> result = fractionalKnapsack(items, 100);
 
